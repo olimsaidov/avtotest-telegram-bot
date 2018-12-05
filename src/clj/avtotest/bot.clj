c734778629048926c0cda09756d03715db6aa4ba
Olim Saidov <olim.mail@gmail.com>
2018-12-05 16:35:34 +0500

    Implemented mvp version

--- src/clj/avtotest/bot.clj
+++ src/clj/avtotest/bot.clj
@@ -1,11 +1,14 @@
 (ns avtotest.bot
-  (:require [morse.handlers :refer :all]
-            [morse.polling :as poll]
+  (:require [morse.api :as t]
             [clojure.edn :as edn]
-            [clojure.java.io :as io]
             [mount.core :as mount]
-            [morse.api :as t]
-            [avtotest.config :refer [env]]))
+            [morse.polling :as poll]
+            [clojure.java.io :as io]
+            [clojure.string :as str]
+            [clj-http.client :as http]
+            [morse.handlers :refer :all]
+            [avtotest.config :refer [env]]
+            [cheshire.core :refer [generate-string]]))
 
 
 (def select-all-text "Барча мавзулар")
@@ -30,87 +33,236 @@
                               sections)))) {})))
 
 
+(mount/defstate counts
+  :start (->> questions
+              (reduce
+                (fn [a question]
+                  (-> a
+                      (update nil (fnil inc 0))
+                      (update (-> question :section :number) (fnil inc 0))
+                      (update [(-> question :section :number) (-> question :sub-section :number)] (fnil inc 0)))) {})))
+
+
+(defonce topics (atom {}))
+
+
 (defn parse-int
   [x]
-  (Integer/parseInt x))
+  (try
+    (Integer/parseInt x)
+    (catch Exception _ nil)))
+
+
+(def digits
+  ["0⃣" "1⃣" "2⃣" "3⃣" "4⃣"
+   "5⃣" "6⃣" "7⃣" "8⃣" "9⃣"])
+
+
+(defn hash-tag [question]
+  (->> [(-> question :section :number)
+        (-> question :sub-section :number)
+        (-> question :number)]
+       (filter identity)
+       (str/join "_")
+       (str "#савол_")))
+
+
+(defn open-question
+  [question chat]
+  (let [text     (str (hash-tag question) " "
+                      (:text question) "\n\n"
+                      (->> (:options question)
+                           (sort-by :number)
+                           (map (fn [{:keys [number text]}]
+                                  (format "%s %s" (nth digits number) text)))
+                           (str/join "\n")))
+        keyboard {:inline_keyboard
+                  [(->> (:options question)
+                        (sort-by :number)
+                        (map (fn [{:keys [number]}]
+                               {:text          number
+                                :callback_data (str (-> question :section :number) "."
+                                                    (-> question :sub-section :number) "."
+                                                    (-> question :number) "."
+                                                    number)})))]}]
+    (if (:image question)
+      (do (http/post
+            (str "https://api.telegram.org/bot" (:bot-token env) "/sendPhoto")
+            {:as        :json
+             :multipart [{:part-name "chat_id" :encoding "UTF-8" :content (str chat)}
+                         {:part-name "photo" :encoding "UTF-8" :content (io/file (io/resource (str "public/img/" (:image question)))) :name "photo.png"}
+                         {:part-name "caption"
+                          :encoding  "UTF-8"
+                          :content   text}
+                         {:part-name "reply_markup"
+                          :encoding  "UTF-8"
+                          :content   (generate-string keyboard)}]}))
+      (t/send-text
+        (:bot-token env)
+        chat
+        {:reply_markup keyboard}
+        text))))
+
+
+(defn close-question
+  [question message chat]
+  (let [text (str (hash-tag question) " "
+                  (:text question) "\n\n✅ "
+                  (->> (:options question)
+                       (filter #(= (:number %) (:correct question)))
+                       (first)
+                       (:text)))]
+    (if (:image question)
+      (http/post
+        (str "https://api.telegram.org/bot" (:bot-token env) "/editMessageCaption")
+        {:content-type :json
+         :as           :json
+         :form-params  {:chat_id    chat
+                        :message_id message
+                        :caption    text}})
+      (t/edit-text
+        (:bot-token env)
+        user
+        message
+        text))))
+
+
+(def standard-keyboard
+  {:resize_keyboard true
+   :keyboard        [["Кейинги савол" "Мазвулар"]]})
+
+
+(defn next-question [chat]
+  (let [topic (get @topics chat)]
+    (when (some? topic)
+      (let [[section sub-section] topic]
+        (cond->> questions
+                 (some? section) (filter #(= section (-> % :section :number)))
+                 (some? sub-section) (filter #(= sub-section (-> % :sub-section :number)))
+                 :then (rand-nth))))))
 
 
 (defhandler bot
+  (callback-fn
+    (fn [{id                    :id
+          {chat :id}            :from
+          {message :message_id} :message
+          data                  :data}]
+      (when id
+        (let [[_ section-number sub-section-number question-number option-number]
+              (re-find #"^(\d+)\.(\d*)\.(\d+)\.(\d+)$" data)
+              question (->> questions
+                            (filter (fn [{:keys [number section sub-section]}]
+                                      (and (= number (parse-int question-number))
+                                           (= (:number section) (parse-int section-number))
+                                           (= (:number sub-section) (parse-int sub-section-number)))))
+                            (first))]
+          (if (= (:correct question) (parse-int option-number))
+            (close-question question message chat)
+            (t/answer-callback (:bot-token env) id "❌ Жавоб нотўғри"))))))
+
   (message-fn
-    (fn [{message :text {user :id} :from}]
-      (cond
-        (= "Мазвулар" message)
-        (t/send-text
-          (:bot-token env)
-          user
-          {:reply_markup
-           {:resize_keyboard true
-            :keyboard        (->> (keys sections)
-                                  (sort-by :number)
-                                  (map #(str (:number %) ". " (:title %)))
-                                  (map vector))}}
-          "Мавзуни танланг:")
-
-        (re-find #"(\d+)\.(\d+)\. (.+)" message)
-        (let [[_ section-number number title] (re-find #"(\d+)\.(\d+)\. (.+)" message)
-              section      (->> (keys sections)
-                                (filter #(= (:number %) (parse-int section-number)))
-                                (first))
-              sub-section  (->> {:number (parse-int number) :title title}
-                                (get (set (sections section))))]
-
+    (fn [{message :text {chat :id} :from}]
+      (when message
+        (cond
+          (= "Кейинги савол" message)
+          (if-let [question (next-question chat)]
+            (open-question (next-question chat) chat)
+            (t/send-text
+              (:bot-token env)
+              chat
+              "Мавзу танлагмаган ⚠️"))
+          (re-matches (re-pattern (str select-all-text " \\(\\d+\\)")) message)
+          (do
+            (swap! topics assoc chat [])
+            (t/send-text
+              (:bot-token env)
+              chat
+              {:parse_mode   "markdown"
+               :reply_markup standard-keyboard}
+              (format "Мавзу танланди: %s\nСаволлар сони: %d" select-all-text (count questions))))
+          (= "Мазвулар" message)
           (t/send-text
             (:bot-token env)
-            user
-            {:parse_mode   "markdown"
-             :reply_markup {:resize_keyboard true
-                            :keyboard        [["Мазвулар"]]}}
-            (format "Мавзу танланди: *%d.%d. %s*" (:number section) (:number sub-section) (:title sub-section))))
-        (re-find #"(\d+)\. (.+)" message)
-        (let [[_ number text] (re-find #"(\d+)\. (.+)" message)
-              section {:number (parse-int number) :title text}]
-          (cond
-            (= select-all-text (:title section))
-            (let [section (->> (keys sections)
-                               (filter #(= (:number section) (:number %)))
-                               (first))]
+            chat
+            {:reply_markup
+             {:resize_keyboard true
+              :keyboard        (->> (keys sections)
+                                    (sort-by :number)
+                                    (map #(str (:number %) ". " (:title %) " (" (counts (:number %)) ")"))
+                                    (map vector)
+                                    (concat [[(str select-all-text " (" (count questions) ")")]]))}}
+            "Мавзуни танланг:")
+
+          (re-find #"^(\d+)\.(\d+)\. (.+) \(\d+\)$" message)
+          (let [[_ section-number number title] (re-find #"^(\d+)\.(\d+)\. (.+) \(\d+\)$" message)
+                section     (->> (keys sections)
+                                 (filter #(= (:number %) (parse-int section-number)))
+                                 (first))
+                sub-section (->> {:number (parse-int number) :title title}
+                                 (get (set (sections section))))]
+
+            (swap! topics assoc chat [(:number section) (:number sub-section)])
+            (t/send-text
+              (:bot-token env)
+              chat
+              {:parse_mode   "markdown"
+               :reply_markup standard-keyboard}
+              (format "Мавзу танланди: *%d.%d. %s*\nСаволлар сони: %d"
+                      (:number section)
+                      (:number sub-section)
+                      (:title sub-section)
+                      (counts [(:number section) (:number sub-section)]))))
+          (re-find #"^(\d+)\. (.+) \(\d+\)$" message)
+          (let [[_ number text] (re-find #"^(\d+)\. (.+) \(\d+\)$" message)
+                section {:number (parse-int number) :title text}]
+            (cond
+              (= select-all-text (:title section))
+              (let [section (->> (keys sections)
+                                 (filter #(= (:number section) (:number %)))
+                                 (first))]
+                (swap! topics assoc chat [(:number section)])
+                (t/send-text
+                  (:bot-token env)
+                  chat
+                  {:parse_mode   "markdown"
+                   :reply_markup standard-keyboard}
+                  (format "Мавзу танланди: *%d. %s*\nСаволлар сони: %d"
+                          (:number section)
+                          (:title section)
+                          (counts (:number section)))))
+              (sections section)
               (t/send-text
                 (:bot-token env)
-                user
-                {:parse_mode "markdown"
-                 :reply_markup {:resize_keyboard true
-                                :keyboard        [["Мазвулар"]]}}
-                (format "Мавзу танланди: *%d. %s*" (:number section) (:title section))))
-            (sections section)
-            (t/send-text
-              (:bot-token env)
-              user
-              {:reply_markup
-               {:resize_keyboard true
-                :keyboard        (->> (sections section)
-                                      (sort-by :number)
-                                      (map #(str (:number section) "." (:number %) ". " (:title %)))
-                                      (map vector)
-                                      (concat [[(str (:number section) ". " select-all-text)]]))}}
-              "Мавзуни танланг:")
-            (contains? sections section)
-            (t/send-text
-              (:bot-token env)
-              user
-              {:parse_mode "markdown"
-               :reply_markup {:resize_keyboard true
-                              :keyboard        [["Мазвулар"]]}}
-              (format "Мавзу танланди: *%d. %s*" (:number section) (:title section))))))))
+                chat
+                {:reply_markup
+                 {:resize_keyboard true
+                  :keyboard        (->> (sections section)
+                                        (sort-by :number)
+                                        (map #(str (:number section) "." (:number %) ". " (:title %) " (" (counts [(:number section) (:number %)]) ")"))
+                                        (map vector)
+                                        (concat [[(str (:number section) ". " select-all-text " (" (counts (:number section)) ")")]]))}}
+                "Мавзуни танланг:")
+              (contains? sections section)
+              (do
+                (swap! topics assoc chat [(:number section)])
+                (t/send-text
+                  (:bot-token env)
+                  chat
+                  {:parse_mode   "markdown"
+                   :reply_markup standard-keyboard}
+                  (format "Мавзу танланди: *%d. %s*\nСаволлар сони: %d"
+                     e     (:number section)
+                          (:title section)
+                          (counts (:number section)))))))))))
 
   (message-fn
     (fn [{{user :id} :from}]
       (t/send-text
         (:bot-token env)
         user
-        {:reply_markup {:resize_keyboard true
-                        :keyboard        [["Мазвулар"]]}}
+        {:reply_markup standard-keyboard}
         "Хуш келибсиз"))))
-
 
 
 (mount/defstate poll
